509af59338a23a397c62c122e3435afdef67de96
raineer <raineerdelarita@gmail.com>
2024-12-24 21:51:30 -0800

    custom sign in

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import {Funnel_Display} from 'next/font/google'
 import Provider from './provider';
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,6 +13,7 @@ const AppFont= Funnel_Display({subsets:['latin']})
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={AppFont.className}
@@ -22,5 +24,6 @@ export default function RootLayout({ children }) {
        
       </body>
     </html>
+    </ClerkProvider>
   );
 }
