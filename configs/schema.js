525146fed22f69a84990d449127cb6199609bb57
raineer <raineerdelarita@gmail.com>
2024-12-27 15:40:55 +0800

    productsTable schema

--- configs/schema.js
+++ configs/schema.js
@@ -1,8 +1,21 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    image: varchar(),
-    email: varchar({ length: 255 }).notNull().unique(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  image: varchar(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const productsTable = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  price: integer().notNull(),
+  category: varchar().notNull(),
+  description: varchar().notNull(),
+  about: varchar(),
+  imageUrl: varchar().notNull(),
+  fileUrl: varchar().notNull(),
+  message: varchar(),
+  createdBy: varchar().notNull().references(() =>usersTable.email),
 });
