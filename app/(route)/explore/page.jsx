e29cc43650e1cb8be0152afd6162d279bdb73a38
raineer <raineerdelarita@gmail.com>
2024-12-30 02:10:08 +0800

    searchInput text

--- app/(route)/explore/page.jsx
+++ app/(route)/explore/page.jsx
@@ -9,6 +9,7 @@ import React, { useEffect, useState } from "react";
 const Explore = () => {
   const [productList, setProductList] = useState([]);
   const [offset, setOffset] = useState(0);
+  const [searchText, setSearchText] = useState("")
 
   useEffect(() => {
     GetProductList();
@@ -18,7 +19,8 @@ const Explore = () => {
     try {
       const result = await axios.post("/api/all-products", {
         limit: 6,
-        offset:offset_
+        offset:offset_,
+        searchInput: searchText,
       });
       console.log("explore result data", result.data);
      
@@ -37,6 +39,17 @@ const Explore = () => {
   return (
     <div className="mt-10 px-4 md:px-8">
       <h2 className="font-bold text-3xl text-center md:text-left">Explore</h2>
+      <div className="mt-6">
+        <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-2 mb-4">
+          <h4 className="text-lg sm:text-base">Search:</h4>
+          <Input className='w-full sm:w-80' placeholder='Search products'
+          onChange={(e)=>setSearchText(e.target.value)}/>
+          <Button onClick={() => {
+            GetProductList(0);
+            setProductList([]);
+          }} className='w-full sm:w-auto mt-2 sm:mt-0'> <Search /> Search</Button>
+        </div>
+      </div>
       <DisplayProductList productsList={productList} />
       <div className="flex items-center justify-center mt-5">
         <Button onClick={()=>GetProductList(offset + 6)} className='w-full sm:w-auto'>Load More</Button>
