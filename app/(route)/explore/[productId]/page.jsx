b43008b2ed579c55f2a74bb7404b91f52e925fac
raineer <raineerdelarita@gmail.com>
2024-12-30 21:27:31 +0800

    productdetail id

--- app/(route)/explore/[productId]/page.jsx
+++ app/(route)/explore/[productId]/page.jsx
@@ -1,4 +1,8 @@
-import { useParams } from 'next/navigation'
+"use client";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import Image from "next/image";
+import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import {
   Accordion,
@@ -8,7 +12,18 @@ import {
 } from "@/components/ui/accordion";
 
 const ProductDetails = () => {
+  const [ProductDetail, setProductDetail] = useState();
   const { productId} = useParams();
+
+  useEffect(() => {
+    GetProductDetail();
+  }, []);
+
+  const GetProductDetail = async () => {
+    const result = await axios.get('/api/products?id='+productId);
+    console.log('result get product id', result.data);
+  }
+
   return (
     <div>ProductDetails</div>
   )
