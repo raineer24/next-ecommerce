bc6cabf63745fbc89aae5ad32d63bc81285393a4
raineer <raineerdelarita@gmail.com>
2025-01-14 14:41:37 +0800

    resend email()

--- app/api/order/route.jsx
+++ app/api/order/route.jsx
@@ -1,7 +1,11 @@
 import { db } from "@/configs/db";
 import { cartTable, OrderTable } from "@/configs/schema";
+import OrderEmail from "@/emails/emails";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req) {
     const { orderDetail, email} = await req.json();
@@ -17,7 +21,21 @@ export async function POST(req) {
     const result = await db.insert(OrderTable).values(orderList);
 
     //Delete user cart items after order
-    const deleteResult = await db.delete(cartTable).where(eq(cartTable.email, email))
+    const deleteResult = await db.delete(cartTable).where(eq(cartTable.email, email));
+
+    //Send Email
+    const sendEmailResult = await SendEmail();
 
     return NextResponse.json(result);
+}
+
+const SendEmail= async ()=> {
+    const result = await resend.emails.send({
+        from: 'raindeer.chickenkiller.com',
+        to: 'delaritaraineer81@gmail.com',
+        subject: 'hello world',
+        react: <OrderEmail/>,
+      });
+
+      return result;
 }
