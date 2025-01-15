5e09b0769eef3f2df8c1c99ef77086c9da449e5f
raineer <raineerdelarita@gmail.com>
2025-01-16 02:27:06 +0800

    emailParams

--- app/api/order/route.jsx
+++ app/api/order/route.jsx
@@ -7,7 +7,7 @@ import { Resend } from "resend";
 import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 import { render } from "@react-email/components";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+//const resend = new Resend(process.env.RESEND_API_KEY)
 
 
 
@@ -35,16 +35,48 @@ export async function POST(req) {
     //Send Email
     const sendEmailResult = await SendEmail(orderDetail, totalAmount);
     console.log('sendemailresult', sendEmailResult);
-    return NextResponse.json(result);
+    return NextResponse.json({result,
+        message:" Order placed and email sent successfully.",
+    });
 }
 
 const SendEmail= async (orderDetail, totalAmount)=> {
-    const result = await resend.emails.send({
-        from: 'Ecommerce email <onboarding@resend.dev>',
-        to: 'delaritaraineer81@gmail.com',
-        subject: 'Order detail',
-        react: <OrderEmail orderDetail={orderDetail} totalAmount={totalAmount} />,
-      });
+    // const result = await resend.emails.send({
+    //     from: 'Ecommerce email <onboarding@resend.dev>',
+    //     to: 'delaritaraineer81@gmail.com',
+    //     subject: 'Order detail',
+    //     react: <OrderEmail orderDetail={orderDetail} totalAmount={totalAmount} />,
+    //   });
 
-      return result;
+    //   return result;
+
+    //render email content dynamically
+    const emailHtml = await render(
+        <OrderEmail orderDetail={orderDetail} totalAmount={totalAmount}/>
+    )
+
+    const mailerSend = new MailerSend({
+        apiKey : process.env.RESEND_API_KEY || "",
+    });
+
+    const sentFrom = new Sender(
+        "onboarding@resend.dev",
+        "raineer"
+    );
+
+    const recipients = [new Recipient(email, 'Your Client')];
+
+    const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setSubject('Order Receipt')
+    .setHtml(emailHtml);
+
+    try {
+        const result = await mailerSend.email.send(emailParams);
+        return result;
+    } catch (error) {
+     console.error('Error sending email:', error);
+     throw new Error('Failed to send email');
+    }
 }
