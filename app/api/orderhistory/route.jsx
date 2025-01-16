7f742ca2c6a98867acdaa7ec5b4d3ddad41887c9
raineer <raineerdelarita@gmail.com>
2025-01-16 18:10:58 +0800

    orderhistory route get

--- app/api/orderhistory/route.jsx
+++ app/api/orderhistory/route.jsx
@@ -5,5 +5,21 @@ import { desc, eq, getTableColumns } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
+    const user = await currentUser();
 
+    if (!user) {
+        return NextResponse.json({ error: 'Unauthorized'}, { status: 401});
+    }
+
+    //query the database for orders and their associated products
+    const result = await db
+        .select({
+            ...getTableColumns(productsTable)
+        })
+        .from(OrderTable)
+        .innerJoin(productsTable, eq(productsTable.id, OrderTable.productId))
+        .where(eq(OrderTable.email, user?.primaryEmailAddress?.emailAddress))
+        .orderBy(desc(OrderTable.id));
+
+        return NextResponse.json(result);
 }
