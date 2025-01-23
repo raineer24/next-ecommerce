3bbab4fb5ee1601ed326f08f6fe7f762f078a198
raineer <raineerdelarita@gmail.com>
2025-01-23 22:35:26 +0800

    setanalytics data

--- app/(route)/dashboard/_components/Analytics.jsx
+++ app/(route)/dashboard/_components/Analytics.jsx
@@ -12,7 +12,10 @@ const Analytics = () => {
       try {
         console.log
         const result = await axios.get('/api/analytics');
-        console.log('result analytics', result);
+        console.log('result analytics', result.data);
+        setAnalyticsData(result.data);
+        setLoading(false);
+
       } catch (error) {
         setError('Failed to fetch data');
         setLoading(false);
@@ -33,36 +36,50 @@ const Analytics = () => {
   }
   return (
     <div className='p-4'>
-      <h2>Product Analytics</h2>
+      <h2 className='font-bold text-xl mt-5'>Product Analytics</h2>
 
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-              <th className='p2 border border-gray-300 text-left'>Product Title</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-              <td className='p2 border border-gray-300'>lorem ipsum</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    {analyticsData.length > 0 ?(
+ <div className='mt-4'>
+ <table className='min-w-full border-collapse border border-gray-300'>
+   <thead>
+     <tr>
+       <th className='p2 border border-gray-300 text-left'>Product Title</th>
+       <th className='p2 border border-gray-300 text-left'>Price ($)</th>
+       <th className='p2 border border-gray-300 text-left'>Category</th>
+       <th className='p2 border border-gray-300 text-left'>Description</th>
+       <th className='p2 border border-gray-300 text-left'>Order ID</th>
+       <th className='p2 border border-gray-300 text-left'>User name</th>
+       <th className='p2 border border-gray-300 text-left'>User Email</th>
+       <th className='p2 border border-gray-300 text-left'>Product Image</th>
+     </tr>
+   </thead>
+   <tbody>
+    {analyticsData.map(item => {
+      const { products, orders, users} = item;
+
+      return (
+        <tr key={orders.id}>
+        <td className='p2 border border-gray-300'>{products.title}</td>
+        <td className='p2 border border-gray-300'>{products.price}</td>
+        <td className='p2 border border-gray-300'>{products.category}</td>
+        <td className='p2 border border-gray-300'>{products.description}</td>
+        <td className='p2 border border-gray-300'>{orders.id}</td>
+        <td className='p2 border border-gray-300'>{users.name}</td>
+        <td className='p2 border border-gray-300'>{users.email}</td>
+        <td className='p2 border border-gray-300'>
+          <img src={products.imageUrl} alt={products.title} className='w-20 h-20 object-cover' />
+        </td>
+      </tr>
+      );
+    })}
+   
+   </tbody>
+ </table>
+</div>
+    ) : (
+      <p className='mt-4 text-gray-500'>No Product analytics data available.</p>
+    )}
+     
     </div>
   )
 }
